1bd59c0cd74a0ca394a8389aa8c230322185109b
Regina Shakirova <regina.shak@yandex.ru>
2021-01-20 11:56:45 +0300

    init

---
+++ main212.java
@@ -0,0 +1,12 @@
+public class main212 {
+    public static void main(String[] args) {
+        int firstAmount = 500;
+        int secondAmount = 5000;
+        int bonus;
+        if (1000 < secondAmount) {
+            bonus = secondAmount / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+    }}
